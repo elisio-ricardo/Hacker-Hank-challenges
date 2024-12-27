a5c4c0d07c1db3b34576d484828afe7730e9a2e1
Elisio <elisio.silva@portoseguro.com.br>
2024-12-27 11:38:50 -0300

    Adicionando desafio TagRegex

--- src/Main.java
+++ src/Main.java
@@ -4,23 +4,5 @@ import java.util.regex.Pattern;
 
 public class Main {
     public static void main(String[] args) {
-
-            Scanner in = new Scanner(System.in);
-            int testCases = Integer.parseInt(in.nextLine());
-            while(testCases>0){
-                String line = in.nextLine();
-
-                Pattern p = Pattern.compile("<(.+)>([^<]+)</\\1>");
-                Matcher texto  = p.matcher(line);
-                if (texto.find()){
-                    System.out.println(texto.group(2));
-                    do { System.out.println(texto.group(2));
-                    } while (texto.find());
-                } else {
-                    System.out.println("None");
-                }
-                testCases--;
-            }
         }
-
 }
---
+++ src/Regex/TagRegex.java
@@ -0,0 +1,28 @@
+package Regex;
+
+import java.util.Scanner;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+public class TagRegex {
+    public static void main(String[] args) {
+
+        Scanner in = new Scanner(System.in);
+        int testCases = Integer.parseInt(in.nextLine());
+        while (testCases > 0) {
+            String line = in.nextLine();
+
+            Pattern p = Pattern.compile("<(.+)>([^<]+)</\\1>");
+            Matcher texto = p.matcher(line);
+            if (texto.find()) {
+                System.out.println(texto.group(2));
+                do {
+                    System.out.println(texto.group(2));
+                } while (texto.find());
+            } else {
+                System.out.println("None");
+            }
+            testCases--;
+        }
+    }
+}
